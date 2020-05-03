623e60924914d59a9f9c05cf563c98715b6edacd
VPKSoft <vpksoft@vpksoft.net>
2020-05-03 08:49:50 +0300

    Some minor refactoring.

--- LangLibTestWinforms/FormMain.cs
+++ LangLibTestWinforms/FormMain.cs
@@ -25,13 +25,7 @@ along with LangLib.  If not, see <http://www.gnu.org/licenses/>.
 #endregion
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Reflection;
 using VPKSoft.LangLib;
@@ -47,6 +41,7 @@ namespace LangLibTestWinforms
             {
                 return;
             }
+
             DBLangEngine.DBName = "LangLibTestWinforms.sqlite";
             if (Utils.ShouldLocalize() != null)
             {
@@ -83,6 +78,44 @@ namespace LangLibTestWinforms
         private void btMessageTest_Click(object sender, EventArgs e)
         {
             MessageBox.Show(DBLangEngine.GetMessage("msgTest", "A test message. The last or (|) character in the string {0} splits the message into message and comment part.|A test message (this is the message comment part).", Environment.NewLine));
+        }
+
+        /// <summary>
+        /// A class to use LangLib for a <see cref="DisplayNameAttribute"/> translation.
+        /// Implements the <see cref="System.ComponentModel.DisplayNameAttribute" />
+        /// </summary>
+        /// <seealso cref="System.ComponentModel.DisplayNameAttribute" />
+        public class LangLibDisplayNameAttribute : DisplayNameAttribute
+        {
+            private readonly string messageName;
+            private readonly string defaultText;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="LangLibDisplayNameAttribute"/> class.
+            /// </summary>
+            /// <param name="messageName">
+            /// The text to look up for a translation from the message resource.
+            /// This is also used as the default value for the <see cref="DisplayName"/> property.
+            /// </param>
+            public LangLibDisplayNameAttribute(string messageName)
+            {
+                this.messageName = messageName;
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="LangLibDisplayNameAttribute"/> class.
+            /// </summary>
+            /// <param name="defaultText">The default text to use as a <see cref="DisplayName"/> property value.</param>
+            /// <param name="messageName">The name of the translated text within the message resource.</param>
+            public LangLibDisplayNameAttribute(string defaultText, string messageName)
+            {
+                this.messageName = messageName;
+                this.defaultText = defaultText;
+            }
+
+            public override string DisplayName => // this might need a try / catch block..
+                // assume the message name to be also the fallback localization..
+                DBLangEngine.GetStatMessage(messageName, defaultText ?? messageName);
         }
     }
 }
--- LangLibTestWinforms/Messages.Designer.cs
+++ LangLibTestWinforms/Messages.Designer.cs
@@ -19,7 +19,7 @@ namespace LangLibTestWinforms {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Messages {
@@ -66,6 +66,15 @@ namespace LangLibTestWinforms {
         internal static string msgTest {
             get {
                 return ResourceManager.GetString("msgTest", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to TranslateMe.
+        /// </summary>
+        internal static string msgTranslateMe {
+            get {
+                return ResourceManager.GetString("msgTranslateMe", resourceCulture);
             }
         }
     }
