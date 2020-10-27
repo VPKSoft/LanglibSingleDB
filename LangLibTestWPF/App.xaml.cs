5a1e4c43073c3573c1ce8a9dbd2bbfc600a11325
VPKSoft <vpksoft@vpksoft.net>
2020-10-27 18:38:10 +0200

    Updated the SQLite dependency.

--- LangLibTestWPF/App.xaml.cs
+++ LangLibTestWPF/App.xaml.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region License
+/*
+LangLib
+
+A program and library for application localization.
+Copyright (C) 2020 VPKSoft, Petteri Kautonen
+
+Contact: vpksoft@vpksoft.net
+
+This file is part of LangLib.
+
+LangLib is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LangLib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with LangLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
